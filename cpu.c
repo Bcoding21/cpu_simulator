641d421110be81d84726a33fd0cd7307b61d5af2
Brandon <brandon.cole@bison.howard.edu>
2018-02-13 20:59:05 -0500

    Added to alu function

--- cpu.c
+++ cpu.c
@@ -10,9 +10,8 @@
 
 #include "cpu.h"
 #include "memory.h"
-
+#include <stdlib.h>
 struct cpu_context cpu_ctx;
-
 
 int fetch( struct IF_ID_buffer *out )
 {
@@ -22,28 +21,48 @@ int fetch( struct IF_ID_buffer *out )
 
 int decode( struct IF_ID_buffer *in, struct ID_EX_buffer *out )
 {
-	struct REG_FILE_input* registerInputs = (struct REG_FILE_input*) malloc(sizeof(struct REG_FILE_input)); // carrys inputs in
+	struct REG_FILE_input* registerInputs = (struct REG_FILE_input*) malloc(sizeof(struct REG_FILE_input)); // holds inputs
+
 	int opcode = in->instruction >> 26; // gets bits 31:26
+
 	setControl(opcode); // set outputs for control units
+	setALUControl(opcode); // set alu control output
 	setMultiplexors(opcode); // set all multiplexor  states
 
+	// set inputs
 	registerInputs->read_reg_1 = (in->instruction >> 20) & 0x1F; // get bits 25:31
 	registerInputs->read_reg_2 = (in->instruction >> 15) & 0x1F; // get bits 20:16
 	registerInputs->write_reg = (in->instruction >> 10) & 0x1F; // gets bits 15:11
 	registerInputs->reg_write = cpu_ctx.CNTRL.reg_write; // set register control signal
+	uint32_t sign_extended_val = in->instruction & 0x7FFF;
 
-	struct REG_FILE_output* registerOutputs = (struct REG_FILE_input*) malloc(sizeof(struct REG_FILE_input)); // carrys outputs out
+	struct REG_FILE_output* registerOutputs = (struct REG_FILE_input*) malloc(sizeof(struct REG_FILE_input)); // holds outputs
+
 	registerFile(registerInputs, registerOutputs);
 
-	out->funct_code = in->instruction & 0x1F;
+    // set outputs
 	out->read_data_1 = registerOutputs->read_data_1;
-	out->read_data_2 = (cpu_ctx.aluMUX.value) ? in->instruction & 0x7FFF : registerOutputs->read_data_2; // if 1 then set to 16 bit sign exteded. else set to output of read reg 2
+	out->read_data_2 = (cpu_ctx.aluMUX.value) ? sign_extended_val : registerOutputs->read_data_2; // if 1 then set to 16 bit sign extended. else set to output of read reg 2
+    out->alu_control = registerOutputs->alu_control;
 	return 0;
 }
 
 int execute( struct ID_EX_buffer *in, struct EX_MEM_buffer *out )
 {
+    struct ALU_INPUT* alu_input = (struct ALU_INPUT*) malloc(sizeof(struct ALU_INPUT)); // holds inputs
 
+    // inputs
+    alu_input->input_1 = in->read_data_1;
+    alu_input->input_2 = in->read_data_2;
+    alu_input->alu_control = in->alu_control;
+
+    struct ALU_OUTPUT* alu_output = (struct ALU_OUTPUT*) malloc(sizeof(struct ALU_OUTPUT)); // hold outputs
+
+    alu(alu_input, alu_output);
+
+    // set outputs
+    out->alu_result = alu_output->alu_result;
+    out->branch_result = alu_output->branch_result;
 	return 0;
 }
 
@@ -66,53 +85,81 @@ int instructionMemory(uint32_t address, struct IF_ID_buffer *out) {
 int registerFile(struct REG_FILE_input* input, struct REG_FILE_output* output) {
 	output->read_data_1 = cpu_ctx.GPR[input->read_reg_1];
 	output->read_data_2 = cpu_ctx.GPR[input->read_reg_2];
+
+	// Calculate ALU control output
+	return 0;
+}
+a
+int alu(struct ALU_INPUT* alu_input, struct ALU_OUTPUT* out){
+
+    // will replace with funct later
+    if (alu_input->alu_control == 0){
+        out->alu_result = alu_input->input_1 & alu_input->input_2;
+    }
+
+    else if(alu_input->alu_control == 1){
+        out->alu_result = alu_input->input_1 | alu_input->input_2;
+    }
+
+    else if(alu_input->alu_control == 2){
+        out->alu_result = alu_input->input_1 + alu_input->input_2;
+    }
+
+    else if(alu_input->alu_control == 7) {
+        out->alu_result = alu_input->input_1 - alu_input->input_2;
+    }
+
+    else if(alu_input->alu_control == 8){
+        uint32_t result = alu_input->input_1 - alu_input->input_2;
+        out->alu_result = (result < 0) ?  1 : 0;
+    }
+
+
+
+    return 0;
 }
 
 
-
-
-
-
-int setControl(uint32_t opcode) {
+int setControl(uint32_t opcode) { // sets control signal outputs
 	if (opcode == 0) { // R-Format
-		cpu_ctx.CNTRL.mem_to_reg = 0;
-		cpu_ctx.CNTRL.reg_write = 1;
-		cpu_ctx.CNTRL.mem_read = 0;
-		cpu_ctx.CNTRL.mem_write = 0;
-		cpu_ctx.CNTRL.branch = 0;
-		cpu_ctx.CNTRL.alu_op = 1;
+		cpu_ctx.CNTRL.mem_to_reg = false;
+		cpu_ctx.CNTRL.reg_write = true;
+		cpu_ctx.CNTRL.mem_read = false;
+		cpu_ctx.CNTRL.mem_write = false;
+		cpu_ctx.CNTRL.branch = false;
+		cpu_ctx.CNTRL.alu_op = 2;
 	}
 
 	else if (opcode == 35) { // LW
-		cpu_ctx.CNTRL.mem_to_reg = 1;
-		cpu_ctx.CNTRL.reg_write = 1;
-		cpu_ctx.CNTRL.mem_read = 1;
-		cpu_ctx.CNTRL.mem_write = 0;
-		cpu_ctx.CNTRL.branch = 0;
+		cpu_ctx.CNTRL.mem_to_reg = true;
+		cpu_ctx.CNTRL.reg_write = true;
+		cpu_ctx.CNTRL.mem_read = true;
+		cpu_ctx.CNTRL.mem_write = false;
+		cpu_ctx.CNTRL.branch = false;
 		cpu_ctx.CNTRL.alu_op = 0;
 	}
 
 	else if (opcode == 43) { // SW
-		cpu_ctx.CNTRL.mem_to_reg = 0;
-		cpu_ctx.CNTRL.reg_write = 0;
-		cpu_ctx.CNTRL.mem_read = 0;
-		cpu_ctx.CNTRL.mem_write = 1;
-		cpu_ctx.CNTRL.branch = 0;
+		cpu_ctx.CNTRL.mem_to_reg = false;
+		cpu_ctx.CNTRL.reg_write = false;
+		cpu_ctx.CNTRL.mem_read = false;
+		cpu_ctx.CNTRL.mem_write = true;
+		cpu_ctx.CNTRL.branch = false;
 		cpu_ctx.CNTRL.alu_op = 0;
 	}
 
 	else { // BEQ
-		cpu_ctx.CNTRL.mem_to_reg = 0;
-		cpu_ctx.CNTRL.reg_write = 0;
-		cpu_ctx.CNTRL.mem_read = 0;
-		cpu_ctx.CNTRL.mem_write = 0;
-		cpu_ctx.CNTRL.branch = 1;
-		cpu_ctx.CNTRL.alu_op = 0;
+		cpu_ctx.CNTRL.mem_to_reg = false;
+		cpu_ctx.CNTRL.reg_write = false;
+		cpu_ctx.CNTRL.mem_read = false;
+		cpu_ctx.CNTRL.mem_write = false;
+		cpu_ctx.CNTRL.branch = true;
+		cpu_ctx.CNTRL.alu_op = 1;
 	}
 }
 
 
-void setMultiplexors(short opcode) {
+void setMultiplexors(short opcode) { // sets multiplexor states
 	if (opcode == 0) {
 		cpu_ctx.regMUX.value = true;
 		cpu_ctx.aluMUX.value = false;
--- cpu.h
+++ cpu.h
@@ -18,7 +18,7 @@ struct Control {
 	bool mem_read;
 	bool mem_write;
 	bool branch;
-	bool alu_op;
+	short alu_op;
 };
 
 struct RegDst_MUX {
@@ -36,11 +36,11 @@ struct MemtoReg_MUX {
 struct cpu_context {
 	uint32_t PC;
 	uint32_t GPR[32];
+	short ALU_CONTORL;
 	struct Control CNTRL;
 	struct RegDst_MUX regMUX;
 	struct ALUSrc_MUX aluMUX;
 	struct MemtoReg_MUX memToRegMUX;
-
 };
 
 extern struct cpu_context cpu_ctx;
@@ -50,72 +50,63 @@ struct IF_ID_buffer {
 	uint32_t next_pc;
 };
 
-struct ID_EX_buffer {
 
-	// inputs
-	short read_reg_1, read_reg_2, write_reg, write_data;
-	bool reg_write;
-	
-	// outputs
-	short read_data_1, read_data_2, funct_code;
-	uint32_t sign_extend_val;
-	
+struct ID_EX_buffer {
+	bool alu_control;
+	uint32_t read_data_1, read_data_2;
+
 };
 
-
 struct EX_MEM_buffer {
+	uint32_t alu_result;
+	uint32_t branch_result;
+	uint32_t write_data;
 
-	// inputs
-	uint32_t input_1;
-	uint32_t input_2;
-	short alu_control;
-
-	// outputs
-	uint32_t ex_result;
 };
 
 struct MEM_WB_buffer {
-
-	// inputs
-	uint32_t address, write_data;
-	bool mem_write, mem_read;
-
-	// outputs
-	uint32_t read_data;
+	uint32_t write_data;
 };
 
+
+// Stages
 int fetch( struct IF_ID_buffer *out);
 int decode( struct IF_ID_buffer *in, struct ID_EX_buffer *out );
 int execute( struct ID_EX_buffer *in, struct EX_MEM_buffer *out );
 int memory( struct EX_MEM_buffer *in, struct MEM_WB_buffer *out );
 int writeback( struct MEM_WB_buffer *in );
 
+// Major Components
 int instructionMemory(uint32_t address, struct IF_ID_buffer *out);
 int registerFile(struct REG_FILE_input*, struct REG_FILE_output*);
-int ALU(struct ALU_INPUT* in, struct ALU_OUTPUT* out);
+int alu(struct ALU_INPUT* in, struct ALU_OUTPUT* out);
+
+// Other Helper
 int setControl(uint32_t);
+void setMultiplexors(short);
 
 
 // Register File
 struct REG_FILE_input {
 	uint32_t read_reg_1, read_reg_2, write_reg, write_data;
 	bool reg_write;
-}; 
+};
 
 struct REG_FILE_output {
 	uint32_t read_data_1, read_data_2;
+	short alu_control;
 };
 
+// ALU I/O
 struct ALU_INPUT {
 	uint32_t input_1, input_2;
-	bool alu_control;
+	short alu_control;
 };
 
 struct ALU_OUTPUT {
 	uint32_t alu_result;
 	bool branch_result;
 };
-
 
 // Data Memory
 struct MEM_INPUT {
